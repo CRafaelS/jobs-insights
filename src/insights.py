242663bdb118e7e2ee7d3ede48c7feec72bf26d0
CRafaelS <carlos.rafael@engenharia.ufjf.br>
2022-09-19 19:28:53 -0300

    feat:filter_by_industry

--- src/insights.py
+++ src/insights.py
@@ -73,21 +73,12 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
+    list_industry = list()
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    for job in jobs:
+        if job["industry"] == industry:
+            list_industry.append(job)
+    return list_industry
 
 
 def get_max_salary(path):
