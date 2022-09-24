5e08c53e05b85e8cb65619f341d19499faaa4480
CRafaelS <carlos.rafael@engenharia.ufjf.br>
2022-09-24 10:14:17 -0300

    test: sort_by_criteria. Req 12

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,9 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+from src.jobs import read
 
 
 def test_sort_by_criteria():
-    pass
+    dict_jobs_values = read("src/jobs.csv")
+    sort_by(dict_jobs_values, "min_salary")
+
+    assert dict_jobs_values[0]["min_salary"] == "19857"
